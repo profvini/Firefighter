20354ec99aeb8c2675cfa5a57d208995b926fd6e
Leonardo Saraiva <leoksaraiva@gmail.com>
2021-12-13 01:01:25 -0300

    Ajuste nos NPCS

--- FireFighter/Assets/Scripts/PanelsScript.cs
+++ FireFighter/Assets/Scripts/PanelsScript.cs
@@ -34,7 +34,7 @@ public class PanelsScript : NetworkBehaviour
     void Start()
     {
         saveLifes = 0;
-        txtSaveLifes.text = "Lifes = " + saveLifes.ToString() + "/4";
+        txtSaveLifes.text = "Lifes = " + saveLifes.ToString() + "/5";
 
         saveBar.SetActive(false);
         saveBartext.SetActive(false);
@@ -49,12 +49,12 @@ public class PanelsScript : NetworkBehaviour
 
     void UISaveLifes(int _Old, int _New)
     {
-        txtSaveLifes.text = "Lifes = " + saveLifes.ToString() + "/4";
+        txtSaveLifes.text = "Lifes = " + saveLifes.ToString() + "/5";
     }
 
     public void addSaveLife()
     {
         saveLifes++;
-        txtSaveLifes.text = "Lifes = " + saveLifes.ToString() + "/4";
+        txtSaveLifes.text = "Lifes = " + saveLifes.ToString() + "/5";
     }
 }
